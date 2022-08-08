d8b526649499c7d1b1737212003b94650e1f7f0d
rfabreu <raabreugomes@gmail.com>
2022-08-08 18:18:39 -0400

    Built the database call for all candidates

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: process.env.MYSQL_HOST,
+        // Your MySQL username:
+        user: process.env.MYSQL_USERNAME,
+        // Your MySQL password
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DBNAME
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 // Default response for any other request (Not Found)
