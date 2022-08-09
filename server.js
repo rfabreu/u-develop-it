405a5439165247ddfedf22a395a80dd31671d183
rfabreu <raabreugomes@gmail.com>
2022-08-09 07:19:26 -0400

    Created query for create operation

--- server.js
+++ server.js
@@ -47,6 +47,19 @@ db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
 });
 
 
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
