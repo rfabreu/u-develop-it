7464346b78f772b0c862d8696bf62043bac0cc67
rfabreu <raabreugomes@gmail.com>
2022-08-15 17:13:20 -0400

    Added candidate route to change their party

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const { resourceLimits } = require('worker_threads');
 const inputCheck = require('./utils/inputCheck');
 require("dotenv").config();
 
@@ -121,6 +122,35 @@ app.get('/api/candidate/:id', (req, res) => {
             message: 'success',
             data: row
         });
+    });
+});
+
+// UPDATE a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
     });
 });
 
