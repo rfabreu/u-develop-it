90d603decdc1febfb16f1a375fe1b8b74c8740e3
rfabreu <raabreugomes@gmail.com>
2022-08-23 08:24:56 -0400

    Created route to register new voter

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -37,4 +37,28 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// ALLOW voters to register
+router.post('/voter', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success',
+            data: body
+        });
+    });
+});
+
 module.exports = router;
